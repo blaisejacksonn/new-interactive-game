e9fbbe9ddd66ad1d0f86edcbefdc3e61023d233f
Blaise Jackson <blaise.jackson12@gmail.com>
2022-10-31 23:15:45 -0400

    setting up grid and snake movement

--- app.js
+++ app.js
@@ -4,5 +4,21 @@ document.addEventListener('DOMContentLoaded',()=>{
     const scoreDisplay = document.querySelector('span')
     const startButton = document.querySelector('.start')
 
+    //grid 
+    const width = 10
+    let currentIndex = 0
+    let appleIndex = 0
+    //setting snake array to move 
+    let currentSnake = [2,1,0]
+    let direction = 1
+    let score = 0
+    let speed = 0.8
+    let intervalTime = 0
+    let interval = 0
+
     
+
+
+
+
 })
