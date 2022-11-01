87a61f0b621da04767468f2d4ac21bef1f219094
Blaise Jackson <blaise.jackson12@gmail.com>
2022-10-31 22:55:33 -0400

    gather html to js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    //selecting html elements
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startButton = document.querySelector('.start')
+
+    
+})
